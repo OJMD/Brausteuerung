dec6bb5519a0e5cf27df5b753492bd6cc2c813e3
OJMD <ojmd@live.de>
2018-03-30 12:33:39 +0200

    es geht weiter

--- Brausteuerung.ino
+++ Brausteuerung.ino
@@ -23,27 +23,27 @@ long Timer[] = {900000,180000,180000,180000};    // Default Zeiten für 0: Einma
 bool CheckTemp[] = {false,false,false,false};     // Alle Prüfungen auf false setzen.
 
 void setup() {
-  
+
   lcd.begin(20,4);
   lcd.backlight();
 
   pinMode(PinRelais1, OUTPUT);
-  
+
 }
 
 void loop() {
 
       //digitalWrite(PinRelais1, EIN);
-      
+
   float temperature = getTemp();
-  
+
   lcd.setCursor(0,0);
   lcd.print("Hallo World...");
   lcd.setCursor(0,1);
   lcd.print("Hallo Steffen...");
   lcd.setCursor(0,2);
   lcd.print(temperature);
-  
+
   lcd.setCursor(0,3);
   lcd.print("\337C");
   lcd.setCursor(19,3);
@@ -51,13 +51,15 @@ void loop() {
 
   delay(1000);
   //lcd.clear();
- 
+
 }
 
 void Einmaischen(){
 
   float t_ist = getTemp();
-  
+
+  long ct; // Current time
+
 
   if(t_ist <= Temp[0]){
     digitalWrite(PinRelais1, EIN);
@@ -67,9 +69,14 @@ void Einmaischen(){
   }
 
   if(CheckTemp[0] = true ){
-    
+    td = msToTime( Timer[0] - millis() );
+  }else{
+    td = msToTime( Timer[0] );
   }
-  
-  
-}
 
+  lcd.setCursor(0,0);
+  lcd.print("Einmaischen:");
+  lcd.setCursor(13,0);
+  lcd.print( td );
+
+}
