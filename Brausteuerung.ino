dd020dc8f853e067b371d8b81a021ff82b6a7177
OJMD <ojmd@live.de>
2018-03-30 18:01:43 +0200

    es geht weiter

--- Brausteuerung.ino
+++ Brausteuerung.ino
@@ -17,78 +17,74 @@ const char EIN = LOW;
 const char AUS = HIGH;
 
 
-/*  */
-float   Temp[] = {45,62,68,72};                     // Default Temperaturen 0: Einmaischen; 1: Rast 1 usw.
-long Timer[] = {900000,180000,180000,180000};    // Default Zeiten für 0: Einmaischen; 1: Rast 1 usw.
-bool CheckTemp[] = {false,false,false,false};     // Alle Prüfungen auf false setzen.
+/*  Sollte in eine Class gepackt werden?*/
+float   Temp[] = {45,62,68,72,72,78,99};                                // Default Temperaturen 0: Einmaischen; 1: Rast 1 usw.
+long    Timer[] = {900000,180000,180000,180000,180000,0,4500000};     // Default Zeiten für 0: Einmaischen; 1: Rast 1 usw.
+bool    CheckTemp[] = {false,false,false,false};                           // Alle Prüfungen auf false setzen.
+String  Name[] = {"Einmaischen:","1. Rast:","2. Rast:","3. Rast:", "4. Rast:", "Laeutern:","Kochen:"};
+
 
 void setup() {
 
+  Serial.begin (9600);
+
   lcd.begin(20,4);
   lcd.backlight();
+  lcd.clear();
 
   pinMode(PinRelais1, OUTPUT);
+
+  digitalWrite(PinRelais1, AUS);
 
 }
 
 void loop() {
 
-      //digitalWrite(PinRelais1, EIN);
-
   float temperature = getTemp();
 
-/*
-
-  lcd.setCursor(0,0);
-  lcd.print("Hallo World...");
-  lcd.setCursor(0,1);
-  lcd.print("Hallo Steffen...");
-  lcd.setCursor(0,2);
-  lcd.print(temperature);
-
-  lcd.setCursor(0,3);
-  lcd.print("\337C");
-  lcd.setCursor(19,3);
-  lcd.print("X");
-
-  delay(1000);
-  //lcd.clear();
- */
-
-  Einmaischen();
-
+  Task( 5 );
+ 
 }
 
-void Einmaischen(){
+void Task(int id){
 
   float t_ist = getTemp();
 
   String ct; // Current time
 
 
-  if(t_ist <= Temp[0]){
+  if(t_ist <= Temp[id]){
     digitalWrite(PinRelais1, EIN);
   }else{
     digitalWrite(PinRelais1, AUS);
-    CheckTemp[0] = true;
+    CheckTemp[id] = true;
+    Serial.write("Check Temp 0 = True \n");
   }
 
-  if(CheckTemp[0] = true ){
-    ct = msToTime( Timer[0] - millis() );
+  if(CheckTemp[id] == true ){
+    ct = msToTime( Timer[id] - millis() );
   }else{
-    ct = msToTime( Timer[0] );
+    ct = msToTime( Timer[id] );
   }
+
+  if( (int)( Timer[id] - millis() ) <= 0 ){
+    ct = "0:00:00";    
+  }
+  
   // Erste Zeile .....
   lcd.setCursor(0,0);
-  lcd.print("Einmaischen:");
+  lcd.print( Name[id] );
   lcd.setCursor(13,0);
-  lcd.print( Temp[0] + String("\337C") );
+  lcd.print( Temp[id] + String("\337C") );
   
   // Zweite Zeile .....
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,2);
   lcd.print("Dauer:");
-  lcd.setCursor(13,0);
+  lcd.setCursor(12,2);
   lcd.print( ct  );
 
+// Vierte Zeile .....
+  lcd.setCursor(13,3);
+  lcd.print( t_ist + String("\337C")  );
 
 }
